d96bb6ceb2798926e4c547eeb84c1184faccd41f
veegas <mohamed.yasser.abdel.razek@gmail.com>
2016-02-16 16:08:21 +0200

    clear

--- game.js
+++ game.js
@@ -784,7 +784,7 @@ function update() {
 
 function draw() {
 
-  ctx.width = ctx.width;
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.drawImage(background, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   // ctx.drawImage(housesImage, 0, CANVAS_HEIGHT - 100, CANVAS_WIDTH, 100);
 
